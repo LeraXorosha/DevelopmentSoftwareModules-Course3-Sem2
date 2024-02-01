15bb70bfaa44e6f45fc1ba9e43dbb4bbfbd9efe7
Valeri <choroshunovalera678@gmail.com>
2024-02-01 15:36:44 +0700

    My user control

---
+++ sourses/Laba1/WinFormsApp/MyForm.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp
+{
+	public partial class MyForm : Form
+	{
+		public MyForm()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
