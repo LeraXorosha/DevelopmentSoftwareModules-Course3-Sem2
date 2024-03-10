5e1b4ad7f24867bde6581a49a4796fc9c6161aaa
Valeri <choroshunovalera678@gmail.com>
2024-03-11 00:48:15 +0700

    create UserControl

---
+++ sourses/Laba3/Laba3/UserControls/MaterialsControl.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Laba3.UserControls
+{
+	partial class MaterialsControl
+	{
+		/// <summary> 
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором компонентов
+
+		/// <summary> 
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			lblIdMaterial = new Label();
+			lblNameMaterial = new Label();
+			tbIdMaterial = new TextBox();
+			tbNameMaterial = new TextBox();
+			btnProductsMaterial = new Button();
+			lblProductsMaterial = new Label();
+			SuspendLayout();
+			// 
+			// lblIdMaterial
+			// 
+			lblIdMaterial.AutoSize = true;
+			lblIdMaterial.Location = new Point(20, 18);
+			lblIdMaterial.Name = "lblIdMaterial";
+			lblIdMaterial.Size = new Size(22, 20);
+			lblIdMaterial.TabIndex = 0;
+			lblIdMaterial.Text = "id";
+			// 
+			// lblNameMaterial
+			// 
+			lblNameMaterial.AutoSize = true;
+			lblNameMaterial.Location = new Point(92, 18);
+			lblNameMaterial.Name = "lblNameMaterial";
+			lblNameMaterial.Size = new Size(49, 20);
+			lblNameMaterial.TabIndex = 1;
+			lblNameMaterial.Text = "Name";
+			// 
+			// tbIdMaterial
+			// 
+			tbIdMaterial.Location = new Point(20, 45);
+			tbIdMaterial.Name = "tbIdMaterial";
+			tbIdMaterial.Size = new Size(50, 27);
+			tbIdMaterial.TabIndex = 2;
+			// 
+			// tbNameMaterial
+			// 
+			tbNameMaterial.Location = new Point(92, 46);
+			tbNameMaterial.Name = "tbNameMaterial";
+			tbNameMaterial.Size = new Size(183, 27);
+			tbNameMaterial.TabIndex = 3;
+			// 
+			// btnProductsMaterial
+			// 
+			btnProductsMaterial.BackColor = Color.FromArgb(192, 255, 192);
+			btnProductsMaterial.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+			btnProductsMaterial.Location = new Point(296, 44);
+			btnProductsMaterial.Name = "btnProductsMaterial";
+			btnProductsMaterial.Size = new Size(94, 29);
+			btnProductsMaterial.TabIndex = 4;
+			btnProductsMaterial.Text = "view";
+			btnProductsMaterial.UseVisualStyleBackColor = false;
+			// 
+			// lblProductsMaterial
+			// 
+			lblProductsMaterial.AutoSize = true;
+			lblProductsMaterial.Location = new Point(296, 18);
+			lblProductsMaterial.Name = "lblProductsMaterial";
+			lblProductsMaterial.Size = new Size(66, 20);
+			lblProductsMaterial.TabIndex = 5;
+			lblProductsMaterial.Text = "Products";
+			// 
+			// MaterialsControl
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			Controls.Add(lblProductsMaterial);
+			Controls.Add(btnProductsMaterial);
+			Controls.Add(tbNameMaterial);
+			Controls.Add(tbIdMaterial);
+			Controls.Add(lblNameMaterial);
+			Controls.Add(lblIdMaterial);
+			Name = "MaterialsControl";
+			Size = new Size(408, 90);
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label lblIdMaterial;
+		private Label lblNameMaterial;
+		private TextBox tbIdMaterial;
+		private TextBox tbNameMaterial;
+		private Button btnProductsMaterial;
+		private Label lblProductsMaterial;
+	}
+}
---
+++ sourses/Laba3/Laba3/UserControls/ProductsControl.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Laba3.UserControls
+{
+	partial class ProductsControl
+	{
+		/// <summary> 
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором компонентов
+
+		/// <summary> 
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			lblPriceProduct = new Label();
+			tbNameProduct = new TextBox();
+			tbIdProduct = new TextBox();
+			lblNameProduct = new Label();
+			lblIdProduct = new Label();
+			tbPriceProduct = new TextBox();
+			cbMaterialProduct = new ComboBox();
+			lblMaterialProduct = new Label();
+			SuspendLayout();
+			// 
+			// lblPriceProduct
+			// 
+			lblPriceProduct.AutoSize = true;
+			lblPriceProduct.Location = new Point(693, 12);
+			lblPriceProduct.Name = "lblPriceProduct";
+			lblPriceProduct.Size = new Size(41, 20);
+			lblPriceProduct.TabIndex = 11;
+			lblPriceProduct.Text = "Price";
+			// 
+			// tbNameProduct
+			// 
+			tbNameProduct.Location = new Point(88, 40);
+			tbNameProduct.Name = "tbNameProduct";
+			tbNameProduct.Size = new Size(580, 27);
+			tbNameProduct.TabIndex = 9;
+			// 
+			// tbIdProduct
+			// 
+			tbIdProduct.Location = new Point(16, 39);
+			tbIdProduct.Name = "tbIdProduct";
+			tbIdProduct.Size = new Size(50, 27);
+			tbIdProduct.TabIndex = 8;
+			// 
+			// lblNameProduct
+			// 
+			lblNameProduct.AutoSize = true;
+			lblNameProduct.Location = new Point(88, 12);
+			lblNameProduct.Name = "lblNameProduct";
+			lblNameProduct.Size = new Size(49, 20);
+			lblNameProduct.TabIndex = 7;
+			lblNameProduct.Text = "Name";
+			// 
+			// lblIdProduct
+			// 
+			lblIdProduct.AutoSize = true;
+			lblIdProduct.Location = new Point(16, 12);
+			lblIdProduct.Name = "lblIdProduct";
+			lblIdProduct.Size = new Size(22, 20);
+			lblIdProduct.TabIndex = 6;
+			lblIdProduct.Text = "id";
+			// 
+			// tbPriceProduct
+			// 
+			tbPriceProduct.Location = new Point(693, 40);
+			tbPriceProduct.Name = "tbPriceProduct";
+			tbPriceProduct.Size = new Size(183, 27);
+			tbPriceProduct.TabIndex = 12;
+			// 
+			// cbMaterialProduct
+			// 
+			cbMaterialProduct.FormattingEnabled = true;
+			cbMaterialProduct.Location = new Point(899, 39);
+			cbMaterialProduct.Name = "cbMaterialProduct";
+			cbMaterialProduct.Size = new Size(183, 28);
+			cbMaterialProduct.TabIndex = 13;
+			// 
+			// lblMaterialProduct
+			// 
+			lblMaterialProduct.AutoSize = true;
+			lblMaterialProduct.Location = new Point(899, 12);
+			lblMaterialProduct.Name = "lblMaterialProduct";
+			lblMaterialProduct.Size = new Size(64, 20);
+			lblMaterialProduct.TabIndex = 14;
+			lblMaterialProduct.Text = "Material";
+			// 
+			// ProductsControl
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			Controls.Add(lblMaterialProduct);
+			Controls.Add(cbMaterialProduct);
+			Controls.Add(tbPriceProduct);
+			Controls.Add(lblPriceProduct);
+			Controls.Add(tbNameProduct);
+			Controls.Add(tbIdProduct);
+			Controls.Add(lblNameProduct);
+			Controls.Add(lblIdProduct);
+			Name = "ProductsControl";
+			Size = new Size(1103, 81);
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label lblPriceProduct;
+		private TextBox tbNameProduct;
+		private TextBox tbIdProduct;
+		private Label lblNameProduct;
+		private Label lblIdProduct;
+		private TextBox tbPriceProduct;
+		private ComboBox cbMaterialProduct;
+		private Label lblMaterialProduct;
+	}
+}
