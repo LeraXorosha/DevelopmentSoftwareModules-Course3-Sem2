fbafb688726bb3d20ad5a90b0e56684b8ce6b9c9
Valeri <choroshunovalera678@gmail.com>
2024-05-30 08:09:05 +0700

    first commit

---
+++ sourses/WPF/Laba7/Laba7/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Laba7.Views;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Laba7
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			LoginWindow loginWindow = new LoginWindow();
+			loginWindow.Show();
+		}
+	}
+
+
+
+}
---
+++ sourses/WPF/Laba7/Laba7/Views/UserInfoView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Laba7.Views
+{
+	/// <summary>
+	/// Логика взаимодействия для UserInfoView.xaml
+	/// </summary>
+	public partial class UserInfoView : Window
+	{
+		public UserInfoView()
+		{
+			InitializeComponent();
+		}
+	}
+}
